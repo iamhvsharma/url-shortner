08f7ac4d79ea1ecf79187c0fb3266b63aea5851a
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-05-25 01:54:25 +0530

    Nanoid generation with length 7

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,27 @@
 import express from "express";
+import { nanoid } from "nanoid";
 
+// Creating an Express application
 const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("Server is running on port http://localhost:3000");
 });
- 
 
 // Sample Route
 app.get("/health", (req, res) => {
   res.send("Health check successful!");
 });
 
+// GET - Redirection
+// POST - Create a short URL
 
-// GET - Redirection 
-// POST - Create a short URL 
+app.post("/api/create", (req, res) => {
+  const { url } = req.body;
+  console.log("Received URL:", url);
+  res.send(nanoid(7));
+});
